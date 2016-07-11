6efe9f93cdfe694d27df33d8e5e0c1056f3944b7
buubre0104 <buu.bre0104@gmail.com>
2016-07-11 11:14:38 +0300

    dao stage 2

--- Jdbc/Reflection/src/main/java/ro/teamnet/zth/appl/dao/DepartmentDao.java
+++ Jdbc/Reflection/src/main/java/ro/teamnet/zth/appl/dao/DepartmentDao.java
@@ -1,7 +1,37 @@
 package ro.teamnet.zth.appl.dao;
 
+import ro.teamnet.zth.api.em.EntityManager;
+import ro.teamnet.zth.api.em.EntityManagerImpl;
+import ro.teamnet.zth.appl.domain.Department;
+
+import java.util.List;
+
+
 /**
  * Created by user on 7/11/2016.
  */
+
 public class DepartmentDao {
+
+    EntityManager emi = new EntityManagerImpl();
+
+    public Department getDepartmentById(Long id){
+        return (Department) emi.findById(Department.class, id);
+    }
+
+    public List<Department> getAllDepartments(){
+        return emi.findAll(Department.class);
+    }
+
+    public Department insertDepartment(Department d){
+        return (Department)emi.insert(d);
+    }
+
+    public Department updateDepartment(Department d){
+        return emi.update(d);
+    }
+
+    public void deleteDepartment(Department d){
+        emi.delete(d);
+    }
 }
