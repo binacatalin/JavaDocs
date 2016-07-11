5d6284e6278fb8140aba8f5005d3f67d089ab900
Bina George Catalin <binacatalin@yahoo.com>
2016-07-11 10:59:31 +0300

    Last Catalin(fara Nicoleta)

--- Jdbc/Reflection/src/test/java/ro/teamnet/zth/api/em/EntityManagerImplTest.java
+++ Jdbc/Reflection/src/test/java/ro/teamnet/zth/api/em/EntityManagerImplTest.java
@@ -11,11 +11,12 @@ import static org.junit.Assert.assertEquals;
  * Created by user on 7/8/2016.
  */
 public class EntityManagerImplTest {
+
     @Test
-    public void testFindAll(){
+    public void testFindAll() {
         EntityManagerImpl emi = new EntityManagerImpl();
         List<Department> result = emi.findAll(Department.class);
-        assertEquals("Department should have 27 entries.", 27, result.size());
+        assertEquals("Department should have 28 entries.", 28, result.size());
 
     }
 
@@ -26,19 +27,18 @@ public class EntityManagerImplTest {
     public void testGetNextIdVal() {
         EntityManagerImpl emi = new EntityManagerImpl();
         Long res = emi.getNextIdVal("EMPLOYEES", "EMPLOYEE_ID");
-        assertEquals((long)207, (long)res);
+        assertEquals((long) 207, (long) res);
     }
 
-    EntityManagerImpl manager = new EntityManagerImpl();
-
     @Test
-    public void testGetTableNameMethod() {
+    public void testInsert() {
+        EntityManagerImpl manager = new EntityManagerImpl();
         Department d = new Department();
         d.setDepartmentName("BlaBla");
-        //d.setLocation(1000l);
+        d.setLocation(1000l);
         d.setId(278L);
 
-        Department res = (Department)(manager.insert(d));
+        Department res = (Department) (manager.insert(d));
         assertEquals(d, res);
     }
 
