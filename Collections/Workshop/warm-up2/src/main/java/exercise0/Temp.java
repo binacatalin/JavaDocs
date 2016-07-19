935e58f3929260f7e97c8c51f12c376ead544cd4
Bina George Catalin <binacatalin@yahoo.com>
2016-07-19 12:10:45 +0300

    DevOps2

---
+++ Collections/Workshop/warm-up2/src/main/java/exercise0/Temp.java
@@ -0,0 +1,13 @@
+package exercise0;
+
+/**
+ * Created by user on 7/19/2016.
+ */
+public class Temp {
+    private Double avg = 2.6;
+    private String msg = "dasdsa";
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
